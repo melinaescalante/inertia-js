e507e5d6998ddbcb48333eb2a07626e324440d59
melina-escalante <meliescalantee@gmail.com>
2024-08-24 20:23:37 -0300

    first

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+    <h1>Estoy cansado jefe</h1>
+</template>
