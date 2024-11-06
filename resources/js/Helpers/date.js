52bee9cc9824ed258db7451d0d06732670df1321
melina-escalante <meliescalantee@gmail.com>
2024-11-05 23:07:07 -0300

    Agregamos la fecha en el bottomsheet de cada posteo

--- resources/js/Helpers/date.js
+++ resources/js/Helpers/date.js
@@ -3,15 +3,24 @@
  * @param date @param{Date|null}
  * @returns  {String}
  */
-export function formatDate(date) {
-    if (!date) return
-    const formatter = new Intl.DateTimeFormat('es-AR', {
+export function formatDate(timestamp) {
+  if (!timestamp) return;
+
+  // Convertir el timestamp a un objeto Date si tiene 'seconds' y 'nanoseconds'
+  let date;
+  if (timestamp.seconds && timestamp.nanoseconds) {
+      date = new Date(timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000);
+  } else {
+      date = new Date(timestamp); // Para otros tipos de fecha ya compatibles
+  }
+
+  const formatter = new Intl.DateTimeFormat('es-AR', {
       day: "2-digit",
       month: "2-digit",
       year: '2-digit',
-      minute: "2-digit",
-      hour: '2-digit', hour12: false
-    })
-    return formatter.format(date).replace(',', ' ')
-    //Formatter para date de creacion de mensaje
-  }
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: false
+  });
+  return formatter.format(date).replace(',', ' ');
+}
