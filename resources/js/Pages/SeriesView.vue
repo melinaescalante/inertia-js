83696c686ca86f6a1fbd0e2abf2d53dfffb0a729
melina-escalante <meliescalantee@gmail.com>
2024-12-28 17:43:17 -0300

    Maquetamos posición del botón de más opciones en la sección del seguimiento de series, en la card de las series viendo actualmente

--- resources/js/Pages/SeriesView.vue
+++ resources/js/Pages/SeriesView.vue
@@ -20,7 +20,7 @@ onMounted(async () => {
         await loadSeriesToWatch()
         await loadSeriesWatching()
         await loadSeriesWatched()
-console.log(loginUser.value)
+        console.log(loginUser.value)
         loading.value = false
     } else {
         loading.value = false
@@ -36,7 +36,7 @@ async function loadSeriesWatched() {
         if (seriesWatched) {
             const lastWatchedSerie = seriesWatched.value.at(seriesWatched.value.lastIndexOf);
             if (lastWatchedSerie) {
-                const response = await fetch('https://api.tvmaze.com/singlesearch/shows?q=' +lastWatchedSerie.nameSerie)
+                const response = await fetch('https://api.tvmaze.com/singlesearch/shows?q=' + lastWatchedSerie.nameSerie)
                 if (response) {
                     const json = await response.json();
                     lastSerieWatched.value = json
@@ -76,7 +76,7 @@ async function loadSeriesToWatch() {
 
 async function loadSeriesWatching() {
     seriesWatching.value = await allSeriesWatching(loginUser.value.id)
- 
+
     if (seriesWatching === false) return
     localSeriesWatching.value = seriesWatching.value;
     try {
@@ -93,7 +93,7 @@ async function loadSeriesWatching() {
             );
             await Promise.all(promises);
         } else {
-          
+
             localSeriesWatching.value = seriesWatching.value;
             localSeriesWatching.value = seriesWatchingJson.value;
         }
@@ -111,7 +111,7 @@ async function next(id, idSerie, nameSerie) {
             seasons = await response.json();
         }
         const { currentEpisode: episode, currentSeason: season, currentIdSeason: idSeason } = await currentInformation(loginUser.value.id, idSerie)
- 
+
         const value = await nextEpisode(id, idSerie, idSeason, season, episode, nameSerie)
         if (value === 'episode') {
 
@@ -168,83 +168,102 @@ async function next(id, idSerie, nameSerie) {
 <template>
     <NavBar></NavBar>
     <section id="mis-series">
-    <div v-if="loading" class="flex justify-center mt-[40vh]">
-        <Spinner>
+        <div v-if="loading" class="flex justify-center mt-[40vh]">
+            <Spinner>
 
-        </Spinner>
-    </div>
-    <template v-else>
-
-        <h1 class="text-xl m-4 skiptranslate" v-if="!seriesWatching || seriesWatchingJson.length===0">
-            Oops, no tienes ni una serie empezada!
-
-        </h1>
-        <h1 v-else class="text-2xl font-medium ms-7 mt-3 skiptranslate">Series empezadas</h1>
-
-        <div class="flex flex-col gap-3 m-4 mt-1 ">
-            <Link href="/buscador" v-if="!seriesWatching || seriesWatchingJson.length===0"
-                class="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 skiptranslate">
-            Empezar
-            nueva serie
-            
-
-            </Link>
-            <div v-for="(serie) in seriesWatchingJson" class="md:m-2 m-1 grid items-center bg-white border border-orange-0 rounded-lg shadow hover:bg-gray-100 
-           sm:grid-cols-1 md:grid-cols-3 md:max-w-xl md:gap-5">
-                <Link :href="`/show/${serie.id}`" class="col-span-2 flex-wrap flex items-center p-2   md:p-1">
-                    <div class="flex flex-col md:flex-row justify-center md:items-center">
-                    
-                <img class="object-cover w-28 h-auto rounded-lg"
-                    :src="serie.image ? serie.image.medium : '/public/noimage.png'"
-                    :alt="`Portada de la última serie en la wishlist ${serie.name}`">
-                <div class="flex flex-col sm:flex-wrap mx-1 md:mx-2">
-                    <h2 class="text-2xl font-normal  tracking-tight text-gray-900">{{ serie.name }}</h2>
-                    <p v-if="localSeriesWatching.find(series => series[serie.id])?.[serie.id].state !== 'end'"
-                        class=" skiptranslate">
-                        Estas viendo de la temporada {{ localSeriesWatching.find(series =>
-                            series[serie.id])?.[serie.id].currentSeason }}
-                        capítulo {{ localSeriesWatching.find(series => series[serie.id])?.[serie.id].current }}
-                    </p>
-                
-                    
-                </div>
-                </div>
-                </Link>
-                <button v-if="localSeriesWatching.find(series => series[serie.id])?.[serie.id].state !== 'end'"
-                    @click.stop="next(loginUser.id, serie.id, serie.name)" type="button" class="text-blue-1000 hover:text-white border border-blue-0 hover:bg-blue-1000  focus:outline-none focus:border-0
-                   focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
-                   md:col-span-1 skiptranslate lg:justify-self-center lg:self-center   md:self-center sm:items-start md:m-2 mb-2 ms-2 self-end sm:m-2 ">
-                    Siguiente
-                </button>
-            </div>
-
-            <h2 class="text-xl m-4 ms-2 skiptranslate" v-if="!seriesToWatch.length >= 1">Oops, no tienes ni una serie en tu lista!</h2>
-
-            <div v-if="!seriesToWatch.length >= 1">
-                <Link href="/buscador"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 block w-full text-center skiptranslate">
-                Agregar
-                serie a wishlist</Link>
-
-            </div>
-            <div v-if="seriesToWatch.length >= 1 && !loading">
-                <h2 class="text-2xl font-medium ms-2 mt-3 mb-3 skiptranslate">Series en tu lista</h2>
-                <CardWithData :data=seriesToWatch :imgContent="lastSerie?.image?.medium"
-                    :altImgContent="lastSerie?.name" text="Tu lista de series para ver" route="/wishlist"
-                    dataName="seriesToWatch">
-                </CardWithData>
-
-            </div>
-            <div v-if="seriesWatched.length >= 1 && !loading">
-
-                <h2 class="text-2xl font-medium ms-2 mt-3 mb-3 skiptranslate">Series vistas</h2>
-
-                <CardWithData :data=seriesWatched :imgContent="lastSerieWatched?.image?.medium"
-                    :altImgContent="lastSerieWatched?.name" text="Tu lista de series vistas" dataName="seriesWatched"
-                    :lastSerieName="lastSerieWatched?.name" route="/seriesVistas">
-                </CardWithData>
-            </div>
+            </Spinner>
         </div>
-    </template>
-</section>
+        <template v-else>
+
+            <h1 class="text-xl m-4 skiptranslate" v-if="!seriesWatching || seriesWatchingJson.length === 0">
+                Oops, no tienes ni una serie empezada!
+
+            </h1>
+            <h1 v-else class="text-2xl font-medium ms-7 mt-3 skiptranslate">Series empezadas</h1>
+
+            <div class="flex flex-col gap-3 m-4 mt-1 ">
+                <Link href="/buscador" v-if="!seriesWatching || seriesWatchingJson.length === 0"
+                    class="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 skiptranslate">
+                Empezar
+                nueva serie
+
+
+                </Link>
+                <div v-for="(serie) in seriesWatchingJson" class="md:m-2 m-1 grid items-center bg-white border border-orange-0 rounded-lg shadow 
+           grid-cols-1 md:grid-cols-4 md:max-w-xl md:gap-5">
+                    <Link :href="`/show/${serie.id}`" class="col-span-3 flex-wrap flex items-center p-2   md:p-1">
+                    <div class="flex flex-col md:flex-row justify-center md:items-center">
+
+
+                        <img class="object-cover w-28 h-auto rounded-lg"
+                            :src="serie.image ? serie.image.medium : '/public/noimage.png'"
+                            :alt="`Portada de la última serie en la wishlist ${serie.name}`">
+                        <div class="flex flex-col sm:flex-wrap mx-1 md:mx-2">
+                            <h2
+                                class="text-2xl font-normal  tracking-tight text-gray-900 hover:text-blue-1000 hover:font-medium">
+                                {{ serie.name }}</h2>
+                            <p v-if="localSeriesWatching.find(series => series[serie.id])?.[serie.id].state !== 'end'"
+                                class=" skiptranslate">
+                                Estas viendo de la temporada {{ localSeriesWatching.find(series =>
+                                    series[serie.id])?.[serie.id].currentSeason }}
+                                capítulo {{ localSeriesWatching.find(series => series[serie.id])?.[serie.id].current }}
+                            </p>
+
+
+                        </div>
+                    </div>
+                    </Link>
+                    <div class="flex md:flex-col flex-row-reverse justify-between space-x-2 col-span-1">
+                        <div class="self-end mb-2 md:mt-0">
+
+                            <button>
+                                <svg class="w-6 h-6  text-gray-800  dark:text-white" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                        d="M12 6h.01M12 12h.01M12 18h.01" />
+                                </svg>
+                            </button>
+
+                        </div>
+                        <button v-if="localSeriesWatching.find(series => series[serie.id])?.[serie.id].state !== 'end'"
+                            @click.stop="next(loginUser.id, serie.id, serie.name)" type="button"
+                            class="text-blue-1000  hover:text-white border border-blue-0 hover:bg-blue-1000  focus:outline-none focus:border-0
+                            focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 text-center 
+                            skiptranslate lg:justify-self-center lg:self-center   md:self-center sm:items-start md:m-2 mb-2 ms-2 md:mt-9 md:mb-10 self-end sm:m-2 ">
+                            Siguiente
+                        </button>
+                    </div>
+                </div>
+
+                <h2 class="text-xl m-4 ms-2 skiptranslate" v-if="!seriesToWatch.length >= 1">Oops, no tienes ni una
+                    serie en tu lista!</h2>
+
+                <div v-if="!seriesToWatch.length >= 1">
+                    <Link href="/buscador"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 block w-full text-center skiptranslate">
+                    Agregar
+                    serie a wishlist</Link>
+
+                </div>
+                <div v-if="seriesToWatch.length >= 1 && !loading">
+                    <h2 class="text-2xl font-medium ms-2 mt-3 mb-3 skiptranslate">Series en tu lista</h2>
+                    <CardWithData :data=seriesToWatch :imgContent="lastSerie?.image?.medium"
+                        :altImgContent="lastSerie?.name" text="Tu lista de series para ver" route="/wishlist"
+                        dataName="seriesToWatch">
+                    </CardWithData>
+
+                </div>
+                <div v-if="seriesWatched.length >= 1 && !loading">
+
+                    <h2 class="text-2xl font-medium ms-2 mt-3 mb-3 skiptranslate">Series vistas</h2>
+
+                    <CardWithData :data=seriesWatched :imgContent="lastSerieWatched?.image?.medium"
+                        :altImgContent="lastSerieWatched?.name" text="Tu lista de series vistas"
+                        dataName="seriesWatched" :lastSerieName="lastSerieWatched?.name" route="/seriesVistas">
+                    </CardWithData>
+                </div>
+            </div>
+        </template>
+    </section>
 </template>
